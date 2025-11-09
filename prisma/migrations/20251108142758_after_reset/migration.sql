49dcdeeba4112b94a49fbbe1f0013dc1460a02ec
inuboeyota <inuboeprog@outlook.com>
2025-11-09 13:30:19 +0900

    [FIX] マイグレーションファイルの更新

---
+++ prisma/migrations/20251108142758_after_reset/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `CommandId` on the `MsgCommandPermission` table. All the data in the column will be lost.
+  - Added the required column `CommandName` to the `MsgCommandPermission` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_MsgCommandPermission" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "CommandName" TEXT NOT NULL,
+    "GuildDiscId" TEXT NOT NULL,
+    "ChannelDiscId" TEXT,
+    "UserDiscId" TEXT,
+    "RoleDiscId" TEXT
+);
+INSERT INTO "new_MsgCommandPermission" ("ChannelDiscId", "GuildDiscId", "RoleDiscId", "UserDiscId", "id") SELECT "ChannelDiscId", "GuildDiscId", "RoleDiscId", "UserDiscId", "id" FROM "MsgCommandPermission";
+DROP TABLE "MsgCommandPermission";
+ALTER TABLE "new_MsgCommandPermission" RENAME TO "MsgCommandPermission";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
