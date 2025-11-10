784bc905740d4190c2e95de869d2ef97e6b438a3
inuboeyota <inuboeprog@outlook.com>
2025-11-10 22:37:54 +0900

    [UPD] Permission完成？これからテストする

--- src/core/messageTriggerCommand/messageCommandPermissions.ts
+++ src/core/messageTriggerCommand/messageCommandPermissions.ts
@@ -15,15 +15,21 @@ export async function messageCommandPermission(message:Message, msgLoadedCommand
 
   // prefixが存在するか
   if (msgLoadedCommands[prefix]) {
+
+    // 存在する場合、コマンド名を取得
+    const commandName:string = msgLoadedCommands[prefix]
+
     // Utilを使用して、発言元の全てのID情報を取得する
     const sentIdInfo:SentIdInfo = await utilGetSentIdInfo(message)
     
     //==prismaの実行========================================================//
 
+    //------------------------------------------
     // 1:prefixで検索して、制限がなければ実行
+    //------------------------------------------
     const isExistPrefix = await prismaInstance.msgCommandPermission.findMany({
       where:{
-        CommandClassName: prefix
+        CommandClassName: commandName
       }
     })
     // 制限がなければ実行
@@ -31,68 +37,92 @@ export async function messageCommandPermission(message:Message, msgLoadedCommand
       return true;
     }
 
-
-    // 2:チャンネルでrestrictが入っている場合、prefix名と発言元のchannel名で検索をかけて取得する
-    // 「チャンネルのリストリクトが存在しない」か「存在する場合にチャンネルIDが一致している」かの判定
-    // 2-1:ChannelRestrictionが存在しない場合、そのまま処理を通過できる。
-    const isAllowedChannel = await prismaInstance.msgCommandPermission.findMany({
+    //--------------------------------
+    // 2:ChannelRestrictionが存在するか
+    //--------------------------------
+    const isExistChannelRestriction = await prismaInstance.msgCommandPermission.findMany({
       where: {
-        AND:[
-          {CommandClassName: prefix},
-          {ChannelDiscId:null}
-        ]
+        CommandClassName: commandName,
+        ChannelDiscId: {not:null}
       }
     })
-    // Channelのリストリクトが存在しない行に該当する場合
-    if (isAllowedChannel.length > 0) {
-      // 2-2:ChannelRestrictionが存在する場合、発言したChannelが一致しているかどうかを判定
-      const isExistChannelRestriction = await prismaInstance.msgCommandPermission.findMany({
+
+    if (isExistChannelRestriction.length === 0) {
+      //---------------------------------------------------------------------------
+      // 2-A:ChannelRestrictionが存在しない場合、UserかRoleが一致すれば実行可能
+      //---------------------------------------------------------------------------
+      const isAllowedCommand = await prismaInstance.msgCommandPermission.findMany({
         where: {
-          AND:[
-            {CommandClassName: prefix},
-            {ChannelDiscId:{not:null, in: sentIdInfo.channelId}}
+          CommandClassName: commandName,
+          OR:[
+            {RoleDiscId: {in:sentIdInfo.roleId}},
+            {UserDiscId: {in:sentIdInfo.userId}}
           ]
         }
       })
-      // Channelのリストリクトが存在し、発言者が当該チャンネルにいない場合は実行不可
-      if (isExistChannelRestriction.length === 0) {
-        message.reply("そのコマンドは実行権限がありません；；")
+
+      if (isAllowedCommand.length === 0) {
+        // Restrictionの許可が存在しない場合
+        message.reply("実行権限が存在しません。")
+        return false;
+      } else {
+        // 1件以上許可が存在する場合
+        return true;
+      }
+
+    }
+    else {
+      //-----------------------------------------------------------------------------------
+      // 2-B:ChannelRestrictionが存在する場合、UserかRoleが一致/もしくは両方NULLなら実行可能
+      //-----------------------------------------------------------------------------------
+      // そもそもこのチャンネルで実行権限が許可されているか
+      const isExistAllowedChannel =  await prismaInstance.msgCommandPermission.findMany({
+        where: {
+          CommandClassName: commandName,
+          ChannelDiscId: {in:sentIdInfo.channelId}
+        }
+      })
+
+      if (isExistAllowedChannel.length === 0) {
+        message.reply("このチャンネルではそのコマンドは実行できません")
+        return false;
+      }
+
+      // RoleもUser両方NULLの場合（=そのチャンネルであればだれでも実行可能）
+      const isAllowedThisChannel =  await prismaInstance.msgCommandPermission.findMany({
+        where: {
+          CommandClassName: commandName,
+          ChannelDiscId: {in:sentIdInfo.channelId},
+          UserDiscId: null,
+          RoleDiscId: null
+        }
+      })
+      // 両方ともNULLなのが1件以上存在すれば実行可能
+      if (isAllowedThisChannel.length > 0) {
+        return true;
+      }
+
+      // ChannelRestrictionが存在する場合であり、UserかRoleのIDが一致すれば実行可能
+      const isAllowedThisChannelAndMember = await prismaInstance.msgCommandPermission.findMany({
+        where:{
+          CommandClassName: commandName,
+          ChannelDiscId: {in:sentIdInfo.channelId},
+          OR: [
+            {UserDiscId:{in:sentIdInfo.userId}},
+            {RoleDiscId:{in:sentIdInfo.roleId}}
+          ]
+        }
+      })
+      // 存在すれば実行可能
+      if (isAllowedThisChannelAndMember.length > 0) {
+        return true;
+      } else {
+        message.reply("このチャンネルでは、あなたに実行権限がありません。");
         return false;
       }
     }
 
-
-    // 3:prefixが存在し、かつChannelのリストリクトにひっかからない場合、prefixの行に送信者のRoleIDかUserIDが含まれているかを判断し、あれば実行（restrictが許可されているとみなす）
-    const isAllowed = await prismaInstance.msgCommandPermission.findMany({
-      where: {
-        AND: [
-          {CommandClassName: prefix},
-          {OR:[
-            {AND: [
-              {ChannelDiscId:{in:sentIdInfo.channelId}},
-              {OR:[
-                  {UserDiscId:{in:sentIdInfo.userId}},
-                  {RoleDiscId:{in:sentIdInfo.userId}}
-                ]
-              }
-            ]},
-            {AND: [
-              {UserDiscId:{in:sentIdInfo.userId}},
-              {RoleDiscId:{in:sentIdInfo.userId}}
-            ]}
-          ]}
-        ]
-      }
-    })
     
-    // 最終判定-存在しなければ
-    if (isAllowed.length === 0) {
-      // 実行しない
-      message.reply("そのコマンドは実行権限がありません。；；")
-      return false;
-    } else {
-      return true;
-    }
 
     //=====================================================================//
 
